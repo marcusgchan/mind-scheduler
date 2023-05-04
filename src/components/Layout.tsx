f80563e5d49d2ecd7d67dfea2fb51cf2abfba0d2
Marcus <marcuscchan@gmail.com>
2023-05-04 08:12:13 -0700

    Changed header color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -235,7 +235,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           )}
         </Disclosure>
 
-        <header className="bg-gray-900 shadow">
+        <header className="bg-indigo-950 shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-white">
               {navigation.find((item) => item.current)?.name ?? ""}
